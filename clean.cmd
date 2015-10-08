d228b527c51840d0c73e0129f4b64e63e3ce14b6
galsok08 <galsok08@gmail.com>
2015-10-08 23:45:05 +0300

    d3

--- clean.cmd
+++ clean.cmd
@@ -4,4 +4,4 @@ pushd C:\Windows\Prefetch && rd /s /q . > nul 2>&1
 pushd c:\Users\Администратор\AppData\Local\Temp && rd /s /q . > nul 2>&1
 pushd c:\Users\Администратор\AppData\Local\Microsoft\Windows\INetCache rd /s /q . > nul 2>&1
 Defrag C: /H /U /V
-pause
+pause111111111111111111
