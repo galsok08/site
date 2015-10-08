cae4786b858490a1c29a18581b92297ef68b5573
galsok08 <galsok08@gmail.com>
2015-10-08 23:26:05 +0300

    commit 4

---
+++ clean.cmd
@@ -0,0 +1,7 @@
+pushd C:\TEMP && rd /s /q . > nul 2>&1
+pushd C:\Windows\TEMP && rd /s /q . > nul 2>&1
+pushd C:\Windows\Prefetch && rd /s /q . > nul 2>&1
+pushd c:\Users\Администратор\AppData\Local\Temp && rd /s /q . > nul 2>&1
+pushd c:\Users\Администратор\AppData\Local\Microsoft\Windows\INetCache rd /s /q . > nul 2>&1
+Defrag C: /H /U /V
+pause
